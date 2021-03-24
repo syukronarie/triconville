09ab817340cd483aae45b795567dbd31c017e441
syukronarie <syukronarie@gmail.com>
2021-03-24 10:37:27 +0700

    fixing all shop

--- src/pages/shop/ShopPage.tsx
+++ src/pages/shop/ShopPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
 import fetchProducts from "../../api/fetchProducts";
 import Products from "../../model/Products";
 import styles from "./components/ShopPage.module.css";
